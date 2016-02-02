c4c1ba144812d21908249f2249193037a82a63b4
Laura Pigott <pigottlaura@gmail.com>
2016-02-02 17:27:28 +0000

    Returning Homepage when requesting index.html

--- index.js
+++ index.js
@@ -2,6 +2,26 @@
 // which it returns in a variable of the same name
 var http = require("http");
 
+// Requiring the File System module, and storing the exports object
+// which it returns in a variable of the same name
+var fs = require("fs");
+
+var resource;
+
+// Reading in the html directory
+fs.readdir("./html", function(err, data){
+  // Logging out the details of errors (if any)
+  if(err){
+    console.log(err);
+  }
+
+  // Looping through each resource in the directory
+  for(d in data){
+    // Logging out the name of each file in the directory
+    console.log(data[d]);
+  }
+});
+
 // Creating a new server, with a callback function that will be
 // called everytime a request is received by the server
 var myServer = http.createServer(function(req, res){
@@ -13,13 +33,19 @@ var myServer = http.createServer(function(req, res){
   // If the requestedResource is the homepage
   if(requestedResource == "/index.html")
   {
+    resource = fs.readFileSync("./html" + requestedResource);
+
     // Adding the content type to the response header
     res.writeHead(200, {"Content-Type" : "text/html"});
-    res.write("HOMEPAGE!!!");
+    res.write(resource);
   }
+  else {
+    // Adding the content type to the response header
+    res.writeHead(404, {"Content-Type" : "text/html"});
 
-  // Writing some HTML to the response body
-  res.write("<h1>Testing my Server</h1><br>You sent a request for " + requestedResource);
+    // Writing some HTML to the response body
+    res.write("<h1>Sorry :(</h1>We couldn't find what you were looking for.<br>You sent a request for " + requestedResource);
+  }
 
   // Sending the response back to the client
   res.end();
